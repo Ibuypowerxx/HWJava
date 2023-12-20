47ab99d7d630f6c9b5e801edd8be349f11e74fc4
ibuypower <danroz1995@yandex.ru>
2023-12-20 22:21:30 +0300

    Mainjava added

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        int x = 10_000; // Стоимость билета
+        int y = 20; // Стоимость одной мили
+        int z = x / y; //Количество бонусных миль
+        System.out.println("Количество начисленных миль за купленный билет  " + z);
+     
+    }
 }
