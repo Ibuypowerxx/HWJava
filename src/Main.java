dcdc96e5f30ec424dbea2d2c6e2d581b4e0ad45c
ibuypower <danroz1995@yandex.ru>
2023-12-23 16:15:38 +0300

    Correct file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketprice = 10_000; // Стоимость билета
-        int milesprice = 20; // Стоимость одной мили
-        int bonusmiles = ticketprice / milesprice; //Количество бонусных миль
-        System.out.println("Количество начисленных миль за купленный билет  " + bonusmiles);
-     
+        int ticketPrice = 10_000; // Стоимость билета
+        int milesPrice = 20; // Стоимость одной мили
+        int bonusMiles = ticketPrice / milesPrice; //Количество бонусных миль
+        System.out.println("Количество начисленных миль за купленный билет  " + bonusMiles);
+
     }
 }
